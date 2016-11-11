56e74e1060f57686aa9e5f9a4169a62b238bfa87
jiao.shen <jiao.shen@qunar.com>
2016-11-11 23:03:30 +0800

    initial

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 'use strict';
-const fs = require('fs');
-const Path = require('path');
-const rimport = /^@?import.*(['"])([^'"]+)\1/;
-const rrequire = /require\s*\((['"])([^'"]+)\1\)/;
-const rignore = /react|react-dom|yo-router|hysdk/;
+var fs = require('fs');
+var Path = require('path');
+var rimport = /^@?import.*(['"])([^'"]+)\1/;
+var rrequire = /require\s*\((['"])([^'"]+)\1\)/;
+var rignore = /react|react-dom|yo-router|hysdk/;
 
 function getScssDependencies(entrance, resolve, context, ignore) {
     if (fs.existsSync(entrance)) {
         if (!ignore) {
             ignore = rignore;
         }
-        const content = fs.readFileSync(entrance, 'utf8');
-        const allLines = content.split(/[\n\r;]+/);
-        let scssDeps = [];
+        var content = fs.readFileSync(entrance, 'utf8');
+        var allLines = content.split(/[\n\r;]+/);
+        var scssDeps = [];
 
-        allLines.forEach((line)=> {
-            let mimport = line.match(rimport);
+        allLines.forEach(function (line) {
+            var mimport = line.match(rimport);
             if (!mimport) {
                 mimport = line.match(rrequire);
             }
 
             if (mimport) {
-                const importPath = mimport[2];
+                var importPath = mimport[2];
                 if (importPath.match(ignore) === null) {
-                    const absImportPath = getAbsImportPath(entrance, importPath, resolve, context);
+                    var absImportPath = getAbsImportPath(entrance, importPath, resolve, context);
                     // 如果是scss, 直接push
                     if (isScssFile(absImportPath)) {
                         scssDeps = scssDeps.concat(getScssDependencies(absImportPath, resolve, context, ignore));
@@ -44,22 +44,22 @@ function getAbsImportPath(entrance, importPath, resolve, context) {
     if (importPath.charAt(0) === '~') {
         importPath = Path.resolve(context, 'node_modules', importPath.slice(1));
     }
-    let ret = importPath;
+    var ret = importPath;
     // 替换alias, 只替换业务代码中的别名
-    const alias = resolve.alias || {};
-    const aliasFound = Object.keys(alias).find(key=> {
-        return importPath.split('/').some(dirname=> {
+    var alias = resolve.alias || {};
+    var aliasFound = Object.keys(alias).find(function(key) {
+        return importPath.split('/').some(function(dirname) {
             return dirname === key;
         });
     });
-    const aliasContent = alias[aliasFound];
-    const isBizFolder = entrance.search('node_modules') === -1;
+    var aliasContent = alias[aliasFound];
+    var isBizFolder = entrance.search('node_modules') === -1;
 
     if (aliasFound && isBizFolder) {
         importPath = importPath.replace(aliasFound, aliasContent);
     }
     // 优先尝试node_modules
-    const nodeModule = tryLoadNodeModules(context, importPath);
+    var nodeModule = tryLoadNodeModules(context, importPath);
     if (nodeModule) {
         ret = nodeModule;
     } else { // 最后尝试普通module
@@ -71,20 +71,20 @@ function getAbsImportPath(entrance, importPath, resolve, context) {
 
 function tryLoadNodeModules(context, dirname) {
     dirname = Path.resolve(context, 'node_modules', dirname);
-    const packageJsonPath = Path.resolve(dirname, 'package.json');
+    var packageJsonPath = Path.resolve(dirname, 'package.json');
     try {
-        let main = 'index.js';
+        var main = 'index.js';
         // 如果找到了package.json,说明是一个npm的包
         if (fs.existsSync(packageJsonPath)) {
-            const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+            var packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
             // 获取包的入口,默认是index.js
             main = packageJson.main || 'index.js';
-            const ret = tryEveryExtname(Path.resolve(dirname, main));
+            var ret = tryEveryExtname(Path.resolve(dirname, main));
             if (ret && fs.existsSync(ret)) {
                 return ret;
             }
         } else { // 反之,是npm包内部的一个文件
-            const ret = tryEveryExtname(Path.resolve(dirname));
+            var ret = tryEveryExtname(Path.resolve(dirname));
             if (ret && fs.existsSync(ret)) {
                 return ret;
             }
@@ -96,15 +96,15 @@ function tryLoadNodeModules(context, dirname) {
 }
 
 function tryEveryExtname(path) {
-    const extnames = ['.js', '.scss', '/index.js', '/index.scss', 'index.js', 'index.scss'];
-    const existedExt = Path.extname(path);
+    var extnames = ['.js', '.scss', '/index.js', '/index.scss', 'index.js', 'index.scss'];
+    var existedExt = Path.extname(path);
     if (existedExt) {
         return path;
     }
 
-    for (let i = 0; i < extnames.length; i++) {
-        const extToBeAppend = extnames[i];
-        const renderedPath = path + extToBeAppend;
+    for (var i = 0; i < extnames.length; i++) {
+        var extToBeAppend = extnames[i];
+        var renderedPath = path + extToBeAppend;
         if (fs.existsSync(renderedPath)) {
             return renderedPath;
         }
@@ -118,8 +118,8 @@ function isScssFile(absPath) {
 }
 
 function group(arr) {
-    let ret = [];
-    for (let i = 0; i < arr.length; i++) {
+    var ret = [];
+    for (var i = 0; i < arr.length; i++) {
         if (ret.indexOf(arr[i]) === -1) {
             ret.push(arr[i]);
         }
@@ -132,10 +132,10 @@ function extractAllScssDependencies(entrance, resolve, context, ignore) {
 }
 
 function combine(entrance, resolve, context, ignore) {
-    let ret = '';
-    const deps = extractAllScssDependencies(entrance, resolve, context, ignore);
-    deps.forEach(dep=> {
-        let content = fs.readFileSync(dep, 'utf8');
+    var ret = '';
+    var deps = extractAllScssDependencies(entrance, resolve, context, ignore);
+    deps.forEach(function(dep) {
+        var content = fs.readFileSync(dep, 'utf8');
         ret += content
             .replace(/@import\s+(['"])[^'"]+\1;?/g, '')
             .replace(/@charset\s+(['"])[^'"]+\1;?/g, '')
