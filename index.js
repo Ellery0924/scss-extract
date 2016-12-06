18ff9ca9dd619d2d1c38089a9d1c743586fe7a40
Ellery0924 <383188743@qq.com>
2016-12-06 11:30:08 +0800

    no es6 code

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+/**
+ * 从js入口中提取所有的scss/css依赖并去重合并
+ * @author jiao.shen
+ */
 'use strict';
 var fs = require('fs');
 var Path = require('path');
-var rimport = /^@?import.*(['"])([^'"]+)\1/;
-var rrequire = /require\s*\((['"])([^'"]+)\1\)/;
-var rignore = /react|react-dom|yo-router|hysdk/;
+var rimport = /@?import.*(['"])([^'"]+)\1/;
+var rrequire = /require(?:\.async)?\s*\((['"])([^'")]+)\1\)/;
+var rignore = /react|react-dom|yo-router|hysdk|babel-polyfill/;
+var rpossibleScssFileName = /([^\/]+\.scss)$/;
+var rsinglelineComment = /\/\/.*(?:[\n\r]|$)/g;
+var rmultilineComment = /\/\s*\*(?:(?!(\*\s*\/)).|[\n\r])*(?:\*\s*\/)/g;
 
 function getScssDependencies(entrance, resolve, context, ignore) {
-    if (fs.existsSync(entrance)) {
+    var entranceExist = fs.existsSync(entrance);
+    if (entranceExist) {
         if (!ignore) {
             ignore = rignore;
         }
-        var content = fs.readFileSync(entrance, 'utf8');
+        var content = fs.readFileSync(entrance, 'utf8')
+            .replace(rsinglelineComment, '')
+            .replace(rmultilineComment, '');
         var allLines = content.split(/[\n\r;]+/);
         var scssDeps = [];
 
@@ -22,10 +32,21 @@ function getScssDependencies(entrance, resolve, context, ignore) {
 
             if (mimport) {
                 var importPath = mimport[2];
-                if (importPath.match(ignore) === null) {
-                    var absImportPath = getAbsImportPath(entrance, importPath, resolve, context);
+                var absImportPath = getAbsImportPath(entrance, importPath, resolve, context);
+                if (absImportPath.match(ignore) === null) {
                     // 如果是scss, 直接push
                     if (isScssFile(absImportPath)) {
+                        // 尝试匹配以下划线开头的文件名
+                        // scss是有病吗...
+                        var alterEntrance = Path.extname(absImportPath) !== '.css' ? absImportPath.replace(rpossibleScssFileName, '_$1') : null;
+                        if (alterEntrance && fs.existsSync(absImportPath) && fs.existsSync(alterEntrance)) {
+                            throw new Error('[ykit-config-fekit]: 编译文件' + absImportPath + '失败，存在文件名冲突（请检查该文件目录下是否存在文件名加了下划线的同名文件）。');
+                        }
+
+                        if (!fs.existsSync(absImportPath) && fs.existsSync(alterEntrance)) {
+                            absImportPath = alterEntrance;
+                        }
+
                         scssDeps = scssDeps.concat(getScssDependencies(absImportPath, resolve, context, ignore));
                         scssDeps.push(absImportPath)
                     } else { // 否则递归查找js依赖中的scss依赖
@@ -97,7 +118,17 @@ function tryLoadNodeModules(context, dirname) {
 }
 
 function tryEveryExtname(path) {
-    var extnames = ['.js', '.scss', '/index.js', '/index.scss', 'index.js', 'index.scss'];
+    var extnames = [
+        '.js',
+        '.scss',
+        '.css',
+        '/index.js',
+        '/index.scss',
+        '/index.css',
+        'index.js',
+        'index.scss',
+        'index.css'
+    ];
     var existedExt = Path.extname(path);
     if (existedExt) {
         return path;
@@ -115,7 +146,12 @@ function tryEveryExtname(path) {
 }
 
 function isScssFile(absPath) {
-    return Path.extname(absPath) === '.scss';
+    // 居然有一种奇葩的写法是require('./'),这里会抛异常
+    try {
+        return Path.extname(absPath) === '.scss' || Path.extname(absPath) === '.css';
+    } catch (e) {
+        return false;
+    }
 }
 
 function group(arr) {
@@ -156,8 +192,8 @@ function combine(entrance, resolve, context, ignore) {
     deps.forEach(function (dep) {
         var content = fs.readFileSync(dep, 'utf8');
         ret += content
-            .replace(/@import\s+(['"])[^'"]+\1;?/g, '')
-            .replace(/@charset\s+(['"])[^'"]+\1;?/g, '')
+            .replace(/@import\s+(?:url\()?(['"])[^'"]+\1(?:\))?;?/g, '')
+            .replace(/@charset\s+(['"])[^'"]+\1;?/g, '');
     });
     return { code: ret, deps: deps };
 }
